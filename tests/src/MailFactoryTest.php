e9c85e6cce94754a34f2993740cf738eb834ff73
Michael Gerzabek <michael.gerzabek@gmail.com>
2021-07-21 11:57:07 +0200

    Überflüssigen Test entfernt

--- tests/src/MailFactoryTest.php
+++ tests/src/MailFactoryTest.php
@@ -4,12 +4,6 @@ namespace Storyfaktor\Mail\Tests;
 
 use Storyfaktor\Mail\Contracts\MailFactory;
 use Symfony\Bridge\Twig\Mime\BodyRenderer;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\Mailer\EventListener\MessageListener;
-use Symfony\Component\Mailer\Mailer;
-use Symfony\Component\Mailer\Transport;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
 
 // https://packagist.org/packages/symfony/mailer
 class MailFactoryTest extends TestCase
@@ -31,36 +25,6 @@ class MailFactoryTest extends TestCase
     $this->assertFalse( $this->factory->exists( 'test', 'html' ) );
     $this->assertFalse( $this->factory->exists( 'test', 'subject' ) );
   }
-
-  /** @test */
-  public function can_factor_html_template_name()
-  {
-    $messageListener = new MessageListener( null, new BodyRenderer( $this->app->get( 'twig' ) ) );
-
-    $eventDispatcher = new EventDispatcher();
-    $eventDispatcher->addSubscriber( $messageListener );
-
-    $transport = Transport::fromDsn( 'smtp://localhost', $eventDispatcher );
-    $mailer = new Mailer( $transport, null, $eventDispatcher );
-
-    $email = $this->factory->create( 'test', [
-      'foo' => 'bar'
-    ] )
-      ->to( 'foo@bar.com' )
-      ->from( 'test@test.at' )
-      ->subject( 'Testing…' );
-
-    /*
-    $mailer->send( $email );
-
-    $this->assertEquals(
-      '<h1>Hey bar, welcome to the show!</h1>',
-      $email->getHtmlTemplate()
-    );
-    */
-    $this->assertTrue( true );
-  }
-
 
   /** @test */
   public function can_render_template()
